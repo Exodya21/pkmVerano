0d93ed4598a7404f0394b61062a5051447933271
Pekexpert07 <carlosgomeztorres.07@gmail.com>
2021-07-03 03:00:01 +0200

    Cambiar archivos a extension PHP.

--- inc/header.php
+++ inc/header.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LockesPKM</title>
+
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+
+    <script src="/js/jquery-3.6.0.min.js"></script>
+    <script src="/js/popper.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
 </head>
 <body>
-    <img src="img/backgroundGEN/3rGEN.jpg" alt="Background">
-    <h1>POKEMON LOCKE</h1>
-
-</body>
-</html>
