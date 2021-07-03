c2f4cfc663bb4b0e97acf2d908eebae2db598aec
Pekexpert07 <carlosgomeztorres.07@gmail.com>
2021-07-03 04:27:19 +0200

    Recuperar código antiguo.

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require 'inc/defines.php';
 
 <?php
 
-if (!empty($_POST)){
+if (!empty($_POST)) {
     $stmt = $mysqli->prepare("INSERT INTO User (nickname, password) VALUES (?, ?)");
     $stmt->bind_param("ss", $_POST['nickname'], $_POST['password']);
     $stmt->execute();
@@ -49,6 +49,135 @@ if (!empty($_POST)){
     </div>
 
 </div>
+
+<h1>POKEMON LOCKE</h1>
+<div>
+    <button>Crear un nuevo perfil</button>
+    <button>Entrar a un perfil</button>
+</div>
+<!-- ----------------- -->
+
+<!-- NUEVO PERFIL -->
+<div class="flexBackground">
+    <img src="img/backgroundGEN/3rGEN.jpg" alt="Background">
+
+    <header>
+        <button>
+            <img /> <!-- ICONO DE FLECHA -->
+        </button>
+        <h1>NUEVO PERFIL</h1>
+    </header>
+
+    <body>
+        <div>
+            <input placeholder="name">
+            <label>nicKname</label>
+        </div>
+
+        <div>
+            <input placeholder="0">
+            <label>Vidas</label>
+        </div>
+
+        <div>
+            <input placeholder="*******">
+            <label>Contraseña</label>
+        </div>
+    </body>
+    <footer>
+        <button> ENVIAR </button>
+    </footer>
+</div>
+
+<!-- ------------ -->
+
+<!-- LOGIN -->
+<div class="flexBackground">
+    <img src="img/backgroundGEN/3rGEN.jpg" alt="Background">
+
+    <header>
+        <button>
+            <img /> <!-- ICONO DE FLECHA -->
+        </button>
+        <h1>LOGIN</h1>
+    </header>
+
+    <body>
+        <div>
+            <input placeholder="name">
+            <label>nicKname</label>
+        </div>
+
+        <div>
+            <input placeholder="*******">
+            <label>Contraseña</label>
+        </div>
+    </body>
+    <footer>
+        <button> ENTRAR </button>
+    </footer>
+</div>
+<!-- ----- -->
+
+<!-- LOCKES -->
+<div class="flexBackground">
+    <img src="img/backgroundGEN/3rGEN.jpg" alt="Background">
+
+    <header>
+        <button>
+            <img /> <!-- ICONO DE FLECHA -->
+        </button>
+        <h1>LOCKES</h1>
+    </header>
+
+    <body>
+        <div>
+            <button>
+                <p>RamDom Iberia</p> <!-- Esto serian variables -->
+                <p> 14 </p>
+                <img> <!-- img corazon con muchos pixeles -->
+            </button>
+
+            <button>
+                + Crear Un Nuevo Locke
+            </button>
+        </div>
+    </body>
+</div>
+<!-- ------ -->
+
+<!-- NUEVO LOCKE -->
+<div class="flexBackground">
+    <img src="img/backgroundGEN/3rGEN.jpg" alt="Background">
+
+    <header>
+        <button>
+            <img /> <!-- ICONO DE FLECHA -->
+        </button>
+        <h1>NUEVO LOCKE</h1>
+    </header>
+
+    <body>
+        <div>
+            <input placeholder="name">
+            <label>Nombre del Locke</label>
+        </div>
+
+        <div>
+            <input placeholder="0">
+            <label>Vidas Iniciales</label>
+        </div>
+
+        <div>
+            <input placeholder="Gen">
+            <label>Generación</label>
+        </div>
+    </body>
+    <footer>
+        <button> CREAR </button>
+    </footer>
+</div>
+-----------
 
 <?php
 require 'inc/footer.php';
